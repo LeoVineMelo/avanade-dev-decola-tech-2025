39a182985878f22a46b914ae54a7c022a69b2eb6
Leonardo Melo <leonardo.melo@cmtech.com.br>
2025-04-06 18:54:51 -0300

    avanade projeto RESTfull Api

---
+++ src/test/java/me/dio/avanade_dev_decola_tech_2025/AvanadeDevDecolaTech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.avanade_dev_decola_tech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeDevDecolaTech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
